90741855aaa7a4389f9e6a5370e7bd6bd20852b2
as-schaudhary <161484534+as-schaudhary@users.noreply.github.com>
2024-07-01 13:56:00 -0400

    until mapping

---
+++ LiL.TimeTracking/Models/TimeTrackingDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LiL.TimeTracking.Models;
+
+public class TimeTrackingDbContext : DbContext
+{
+    public TimeTrackingDbContext()
+    {}
+    public TimeTrackingDbContext(DbContextOptions options): base(options)
+    {}
+
+     public DbSet<Employee> Employees { get; set; }
+     public DbSet<Project> Projects { get; set; }
+     public DbSet<TimeEntry> TimeEntries { get; set; }
+
+}
---
+++ LiL.TimeTracking/Resources/TimeTrackingResources.cs
@@ -0,0 +1,6 @@
+namespace LiL.TimeTracking.Resources;
+
+public record Employee (int Id, string Name, DateOnly StartDate);
+public record Project (int Id, string Name, DateTime StartDate, DateTime? EndDate);
+public record TimeEntry (Guid Id, int EmployeeId, int ProjectId, DateTime DateWorked, decimal HoursWorked);
+public record ProjectAssignment (int EmployeeId, int ProjectId, string? EmployeeName, string? ProjectName);
